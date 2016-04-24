2a1ef1c949f8cba6d943b2d5baeb0dd017f25420
Glaceon31 <grit31@126.com>
2016-04-24 16:24:34 +0800

    m

--- experiments/nmt/theanotest.py
+++ experiments/nmt/theanotest.py
@@ -20,11 +20,16 @@ bc0 = TT.tensor3()
 fc1 = TT.tensor3()
 bc1 = TT.tensor3()
 
-c0 = Concatenate(axis=2)(*[fc0,bc0])
+c0 = Concatenate(axis=2)(*[fc0,bc0]).out
+c1 = Concatenate(axis=2)(*[fc1,bc1]).out
 
-func = theano.function(inputs=[fc0, bc0], outputs=[c0])
+c = Concatenate(axis=1)(*[c0,c1]).out
+
+func = theano.function(inputs=[fc0, bc0,fc1,bc1], outputs=[c0,c1,c])
 
 fc0v = numpy.asarray([[[.1,.1,.1],[.2,.2,.3]],[[.3,.3,.3],[.4,.4,.4]]], dtype=theano.config.floatX)
 bc0v = numpy.asarray([[[.5,.5,.5],[.6,.6,.6]],[[.7,.8,.8],[.8,.8,.8]]], dtype=theano.config.floatX)
+fc1v = numpy.asarray([[[1.1,1.1,1.1],[1.2,1.2,1.3]],[[1.3,1.3,1.3],[1.4,1.4,1.4]]], dtype=theano.config.floatX)
+bc1v = numpy.asarray([[[1.5,1.5,1.5],[1.6,1.6,1.6]],[[1.7,1.8,1.8],[1.8,1.8,1.8]]], dtype=theano.config.floatX)
 
-print func(fc0v,bc0v)
+print func(fc0v,bc0v,fc1v,bc1v)
