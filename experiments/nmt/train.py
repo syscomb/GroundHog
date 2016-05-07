fecba95bf22dd0e21a240fabd08d603fa8f465f9
Glaceon31 <grit31@126.com>
2016-05-08 01:45:00 +0800

    m

--- experiments/nmt/train.py
+++ experiments/nmt/train.py
@@ -19,6 +19,39 @@ logger = logging.getLogger(__name__)
 
 class RandomSamplePrinter(object):
 
+    def __init__(self, state, model, train_iter):
+        args = dict(locals())
+        args.pop('self')
+        self.__dict__.update(**args)
+
+    def __call__(self):
+        def cut_eol(words):
+            for i, word in enumerate(words):
+                if words[i] == '<eol>':
+                    return words[:i + 1]
+            raise Exception("No end-of-line found")
+
+        sample_idx = 0
+        while sample_idx < self.state['n_examples']:
+            batch = self.train_iter.next(peek=True)
+            xs, ys = batch['x'], batch['y']
+            for seq_idx in range(xs.shape[1]):
+                if sample_idx == self.state['n_examples']:
+                    break
+
+                x, y = xs[:, seq_idx], ys[:, seq_idx]
+                x_words = cut_eol(map(lambda w_idx : self.model.word_indxs_src[w_idx], x))
+                y_words = cut_eol(map(lambda w_idx : self.model.word_indxs[w_idx], y))
+                if len(x_words) == 0:
+                    continue
+
+                print "Input: {}".format(" ".join(x_words))
+                print "Target: {}".format(" ".join(y_words))
+                self.model.get_samples(self.state['seqlen'] + 1, self.state['n_samples'], x[:len(x_words)])
+                sample_idx += 1
+
+class RandomSamplePrinter_multi(object):
+
     def __init__(self, state, model, train_iter, enc_dec):
         args = dict(locals())
         args.pop('self')
@@ -106,13 +139,18 @@ def main():
     print 'lm model inputs:', lm_model.inputs
 
     logger.debug("Load data")
-    train_data = get_batch_iterator_multi(state)
+    if state['syscomb']:
+        train_data = get_batch_iterator_multi(state)
+        sampler = RandomSamplePrinter_multi(state, lm_model, train_data, enc_dec)
+    else:
+        train_data = get_batch_iterator(state)
+        sampler = RandomSamplePrinter(state, lm_model, train_data, enc_dec)
     logger.debug("Compile trainer")
     algo = eval(state['algo'])(lm_model, state, train_data)
     logger.debug("Run training")
     main = MainLoop(train_data, None, None, lm_model, algo, state, None,
             reset=state['reset'],
-            hooks=[RandomSamplePrinter(state, lm_model, train_data, enc_dec)]
+            hooks=[sampler]
                 if state['hookFreq'] >= 0
                 else None)
     if state['reload']:
