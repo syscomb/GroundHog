728d6b2438fe57d480b179f4dbfc00837a1ac88b
Glaceon31 <grit31@126.com>
2016-05-15 21:36:27 +0800

    debugbs

--- experiments/nmt/sample_syscomb.py
+++ experiments/nmt/sample_syscomb.py
@@ -55,8 +55,8 @@ class BeamSearch(object):
                 x.append(seq[last_split+1:i+1])
                 last_split = i
         assert self.num_systems == len(x)
-        for i in xrange(self.num_systems):
-            x[i][-1]=self.eos_id
+        #for i in xrange(self.num_systems):
+        #    x[i][-1]=self.eos_id
         c = self.comp_repr(*x)#[0]
         print len(c)
         print c[0].shape
