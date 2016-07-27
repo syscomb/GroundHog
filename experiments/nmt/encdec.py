935260858705ea216e6fbde68dcaead3561a32d1
admin <admin>
2016-07-27 10:52:30 +0800

    edrop

--- experiments/nmt/encdec.py
+++ experiments/nmt/encdec.py
@@ -3019,11 +3019,11 @@ class MultiInputLayer(Layer):
         if self.mean:
             if dropout_encoder:
                 print '\nencoder-level hidden layer dropout\n'
-                dropout_encoder[0] = 1.
                 result = list_inputs[0]*dropout_encoder[0]
                 for i in range(1,self.num_inputs):
                     result += list_inputs[i]*dropout_encoder[i]
-                result /= dropout_encoder.sum()
+                desum = T.switch(T.eq(dropout_encoder.sum(), 0.), 1, dropout_encoder.sum())
+                result /= desum
             else:
                 result = list_inputs[0]
                 for i in range(1,self.num_inputs):
